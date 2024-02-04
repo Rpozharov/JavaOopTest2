ac8446afaa1f401d22fb74e9b458beb689b083ff
Rpozharov <rom.pozharov@gmail.com>
2024-02-04 10:26:03 +0700

    1 commit

--- RangeTest/src/ru/academits/pozharov/range_main/RangeMain.java
+++ RangeTest/src/ru/academits/pozharov/range_main/RangeMain.java
@@ -6,8 +6,10 @@ public class RangeMain {
     public static void main(String[] args) {
         int[] array1 = {2, 5, 8, 9, 0};
         int[] array2 = {};
+        int[] array3 = {2, 8};
 
         System.out.println("array1 = " + Arrays.toString(array1));
         System.out.println("array2 = " + Arrays.toString(array2));
+        System.out.println("array3 = " + Arrays.toString(array2));
     }
 }
